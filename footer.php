d025b7ce3509af6f11f1d2687704f08aa162f770
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-01-28 19:28:38 -0300

    Arquivos iniciais

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 </body>
 <?php wp_footer(); ?>
+<script src="<?= get_template_directory_uri(); ?>/assets/js/bootstrap.bundle.min.js"></script>
 
 </html>
--- header.php
+++ header.php
@@ -13,3 +13,24 @@
 </head>
 
 <body <?= body_class(); ?>>
+
+	<header class="container header">
+		<nav class="navbar navbar-expand-md">
+			<div class="container-fluid">
+				<a class="navbar-brand fs-1" href="/">
+					<?= get_bloginfo(); ?>
+				</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+					aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav justify-content-end w-100">
+						<li class="nav-item">
+							<a class="nav-link fs-5 btn" href="/minha-conta">Login</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
